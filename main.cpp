24962ec2f1bf1516db0d9515c2080628cd06e065
Iain <icleen@gmail.com>
2016-11-20 12:11:30 -0700

    Added the main.cpp file

---
+++ main.cpp
@@ -0,0 +1,20 @@
+/*
+ * Iain Lee
+ * 11-20-16
+ * main.cpp
+ */
+
+#include <iostream>
+#include <vector>
+#include <set>
+#include <sstream>
+#include <fstream>
+
+using namespace std;
+
+int main () {
+
+
+
+	return 0;
+}
